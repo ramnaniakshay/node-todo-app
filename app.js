7980aabf04a05b10fd70ba215b11ee47cc34f276
Akshay Ramnani <60702445+ramnaniakshay@users.noreply.github.com>
2025-06-03 20:52:15 +0530

    Update app.js
    changed port from 80 to 8081

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express'),
     // Mitigate XSS using sanitizer
     sanitizer = require('sanitizer'),
     app = express(),
-    port = 80
+    port = 8081
 
 app.use(bodyParser.urlencoded({
     extended: false
